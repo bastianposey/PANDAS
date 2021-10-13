26a59bcda0c9ed91f492f7a06ba279191cfc3af7
Bastian Posey <bastian_posey2@baylor.edu>
2021-10-13 17:18:13 -0500

    thursday week8

---
+++ pandasDataFrame.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+grades_dict = { 'Wally': [87,96,70],
+                'Eva':[100,87,90],
+                'Sam':[94,77,90],
+                'Katie':[100,81,82],
+                'Bob':[83,65,85]}
+
+grades = pd.DataFrame(grades_dict)
+
+grades.index = ['Test1', 'Test2', 'Test3']
+
+#print(grades)
+
+#print(grades['Eva'])
+
+#print(grades.Sam)
+# using the loc and iloc methods
+
+#print(grades.loc['Test2'])
+
+#print(grades.iloc[1])
+
+
+# For consecutive rows
+#print(grades.loc['Test1':'Test3'])
+
+#print(grades.iloc[:3])
+
+#for non-consecutive rows
+#print(grades.loc[['Test1','Test3']])
+#print(grades.iloc[[0,2]])
+
+#View only Eva's and Katie's grades for Test1 and Test2
+#print(grades.Eva.loc[['Test1','Test2']])
+#print(grades.Katie.loc[['Test1','Test2']])
+#print("")
+#View only Sam's THRU Bob's grades for Test1 and Test3
+print(grades.Sam.loc["Test1":"Test3"])
+#print(grades.Bob.loc["Test1":"Test3"])
+
+print(grades.loc[['Test1', 'Test3'], "Sam":"Bob"])
+
+grades_A = grades[grades >= 90]
+
+print(grades_A)
+
+# create a dataframe for everyone wiht a B grade
+grades_B = [(grades>=80) & (grades < 90)]
+print(grades_B)
+# creates a dataframe for everyone with a A or B grade
+grades_A_or_B = [(grades>=90) | (grades>-80)]
+
+print(grades_A_or_B)
+
+pd.set_option('precision',2)
+
+#print(grades.T.describe())
+
+print(grades.sort_index(ascending=False))
+
+print(grades.sort_index(axis=1, ascending=False))
+
+
+print(grades.sort_values(by='Test1', axis=1,ascending=False))
+
+print(grades.T.sort_values(by='Test1', ascending=False))
+
+print(grades.loc['Test1'].sort_values(ascending=False))
